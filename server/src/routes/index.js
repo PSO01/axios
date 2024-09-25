32c7b9740302307fda5420c84a67252eaf5fd993
PSO01 <rndrmrqkaqka@naver.com>
2024-09-26 01:36:37 +0900

    API 서버 테스트

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -4,9 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 const userRoute = require("./users");
-const postRoute = require("./posts");
 
 router.use('/', userRoute);
-router.use('/posts', postRoute);
 
 module.exports = router;
--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -4,11 +4,9 @@
 const express = require("express");
 const router = express.Router();
 
-// 컨트롤러
 const userCtrl = require("../controllers/user.ctrl");
 const refreshCtrl = require("../controllers/refresh.ctrl");
 const authToken = require("../middlewares/authJWT");
-
 
 router.post("/login", userCtrl.process.login);
 router.delete("/logout",userCtrl.process.logout);
@@ -17,8 +15,7 @@ router.post("/tokens", refreshCtrl);
 
 // 테스트용 라우터들
 router.get("/test",authToken, (req,res)=>{
-    res.json("인증된 사용자만 접근 허용")
-});
-
+    res.send("인증된 사용자만 접근 허용")
+})
 
 module.exports = router;
